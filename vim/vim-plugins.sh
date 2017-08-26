41c16143e893708e54a704329b8fd27407b3705c
marcoswebermw <marcoswebermw@gmail.com>
2017-08-26 19:17:25 -0300

    Create vim-plugins.sh

---
+++ vim/vim-plugins.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+# Instalando o pathogen que serve para instalar os outros plugins.
+mkdir -p ~/.vim/autoload ~/.vim/bundle && \
+curl -LSso ~/.vim/autoload/pathogen.vim https://tpo.pe/pathogen.vim
+
+# Copia o arquivo .vimrc do diretório atual para o diretório ~/.vimrc.
+cp .vimrc ~/.vimrc
+
+# Instalando o NerdTree para gerenciar os diretórios dentro do Vim.
+git clone https://github.com/scrooloose/nerdtree.git ~/.vim/bundle/nerdtree
+
+
+# Este arquivo precisa de permissão de execução. 'chmod +x vim-plugins.sh'.
