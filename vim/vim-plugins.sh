a2c7a61d8a85c3695d2b88fbaf80779cbbd89d7d
marcoswebermw <marcoswebermw@gmail.com>
2017-08-30 17:09:52 -0300

    Update vim-plugins.sh

--- vim/vim-plugins.sh
+++ vim/vim-plugins.sh
@@ -10,5 +10,7 @@ cp .vimrc ~/.vimrc
 # Instalando o NerdTree para gerenciar os diretórios dentro do Vim.
 git clone https://github.com/scrooloose/nerdtree.git ~/.vim/bundle/nerdtree
 
+#  Instalando o plugin para .editorconfig no vim.
+git clone https://github.com/editorconfig/editorconfig-vim.git ~/.vim/bundle/editorconfig-vim
 
 # Este arquivo precisa de permissão de execução. 'chmod +x vim-plugins.sh'.
